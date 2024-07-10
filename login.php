bcd2e77288e57682f8d0568f97dfc65e067dc169
Sahil Patel <jonyydeep555@gmail.com>
2024-07-10 10:52:08 +0530

    commited by sahil

--- login.php
+++ login.php
@@ -50,7 +50,7 @@ if(isset($_POST['submit'])){
       <input type="email" name="email" placeholder="enter email" class="box" required>
       <input type="password" name="password" placeholder="enter password" class="box" required>
       <input type="submit" name="submit" value="login now" class="btn">
-      <p>don't have an account? <a href="register.php">regiser now</a></p>
+      <p>don't have an account? <a href="register.php">register now</a></p>
    </form>
 
 </div>
