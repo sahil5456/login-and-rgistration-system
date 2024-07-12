435e0fc695dd2309033689119108d4c137ce136e
Sahil Patel <jonyydeep555@gmail.com>
2024-07-12 12:58:12 +0530

    commit by sahil

--- register.php
+++ register.php
@@ -13,24 +13,25 @@ if(isset($_POST['submit'])){
    $image_tmp_name = $_FILES['image']['tmp_name'];
    $image_folder = 'uploaded_img/'.$image;
 
-   $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+   // Check if the email already exists
+   $select_email = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email'") or die('query failed');
 
-   if(mysqli_num_rows($select) > 0){
-      $message[] = 'user already exist'; 
-   }else{
+   if(mysqli_num_rows($select_email) > 0){
+      $message[] = 'email already exists'; 
+   } else {
       if($pass != $cpass){
          $message[] = 'confirm password not matched!';
-      }elseif($image_size > 2000000){
+      } elseif($image_size > 2000000){
          $message[] = 'image size is too large!';
-      }else{
+      } else {
          $insert = mysqli_query($conn, "INSERT INTO `user_form`(name, email, password, image) VALUES('$name', '$email', '$pass', '$image')") or die('query failed');
 
          if($insert){
             move_uploaded_file($image_tmp_name, $image_folder);
             $message[] = 'registered successfully!';
             header('location:login.php');
-         }else{
-            $message[] = 'registeration failed!';
+         } else {
+            $message[] = 'registration failed!';
          }
       }
    }
